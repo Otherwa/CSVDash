bd0bf293237131b586d985df1ba609bc89d1dc2d
Atharv Desai <atharvdesai2002@gmail.com>
2023-09-23 21:09:14 +0530

    Otherwa (#4)
    * /updates + column qual -> quan, graph + /
    
    * /file+ dir setup 30%/
    
    * /test_data/
    
    * /changes/
    
    * /line/
    
    ---------
    
    Co-authored-by: Otherwa <67428572+Atharv2002@users.noreply.github.com>

--- src/home.py
+++ src/home.py
@@ -8,7 +8,10 @@ def page_home():
     # Define the Streamlit app title and a brief description
     st.title("CSVdash - Convert & Visualize CSV Data")
     st.write("Welcome to CSVdash, your tool for converting and visualizing CSV data!")
-
+    st.write(
+        "Anime_CSV [link](https://github.com/Otherwa/CSVDash/blob/Otherwa/data/Anime_Analysis.csv)"
+    )
+    
     st.title("UI Changes:")
     st.write("1. Updated the color scheme for a more modern look.")
     st.write("2. Reorganized the navigation menu for better user flow.")
