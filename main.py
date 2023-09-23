98db7833e6a4cd7f6915d0ed43baa725caa2a303
Atharv Desai <atharvdesai2002@gmail.com>
2023-09-23 21:02:53 +0530

    Otherwa (#2)
    * /updates + column qual -> quan, graph + /
    
    * /file+ dir setup 30%/
    
    ---------
    
    Co-authored-by: Otherwa <67428572+Atharv2002@users.noreply.github.com>

--- main.py
+++ main.py
@@ -1,75 +1,139 @@
-# custom dependecies
-from css.css import myStyle
+# Custom dependencies
+from config.css import load_custom_css
+from src.sidebar import create_sidebar
 
-# dependencies
+# pages
+from src.file import page_file
+from src.home import page_home
+
+# Dependencies
 import streamlit as st
 import pandas as pd
+import base64
 
 
 def main():
-    # config
-    st.markdown(myStyle, unsafe_allow_html=True)
+    # Config
+    load_custom_css()
     st.set_option("deprecation.showfileUploaderEncoding", False)
 
-    # Add a sidebar for navigation
-    st.sidebar.title("CSVDash")
-    page = st.sidebar.selectbox("Pages:", ["Home", "Upload CSV"])
+    # Create sidebar and get page and uploaded file
+    page, uploaded_file = create_sidebar()
 
     if page == "Home":
-        # Home page content
-        st.title("Welcome to CSV File Viewer")
-        st.write(
-            "This app allows you to upload a CSV file and view its contents in a table."
-        )
-        # Display a GIF from a URL
-        st.image(
-            "https://i.pinimg.com/originals/e6/10/9e/e6109e32a9ac1a8f2496d7fba78e9c84.gif"
-        )
+        page_home()
+    elif page == "File":
+        page_file(uploaded_file)
 
-    elif page == "Upload CSV":
-        # File upload page
-        st.title("Upload a CSV File")
-        st.write("Upload a CSV file below:")
-        uploaded_file = st.file_uploader("Choose a CSV file", type=["csv"])
+    elif page == "Convert Column":
+        # Convert column to quantitative data page
+        st.title("Convert Column to Quantitative Data")
+        st.write("Select a column and specify replacement options:")
 
         if uploaded_file is not None:
-            # Read the uploaded CSV file into a DataFrame
             df = pd.read_csv(uploaded_file)
+            selected_column = st.selectbox("Select a column:", df.columns)
+            if selected_column:
+                st.write("Unique Non-Quanitative Data:")
+                st.write([*df[selected_column].unique()])
+                # Ask the user for replacement options as a list of tuples (old_value, new_value)
+                st.write(
+                    "Specify replacement options as 'old_value', 'new_value' for new value to ';'"
+                )
+                replacement_options = st.text_input(
+                    "Enter replacement options (comma-separated):"
+                )
+                replacement_options = [
+                    tuple(map(str.strip, r.split(",")))
+                    for r in replacement_options.split(";")
+                ]
+                st.write(replacement_options)
 
-            # Display the DataFrame as a table
-            st.write("Table View:")
-            st.write(df)
+            if st.button("Convert Column"):
+                try:
+                    # Apply user-defined replacements
+                    for old_value, new_value in replacement_options:
+                        df[selected_column] = df[selected_column].replace(
+                            old_value, new_value
+                        )
 
-            # Allow users to select columns for analysis
-            selected_columns = st.multiselect(
-                "Select columns for analysis:", df.columns
-            )
+                    st.write("Updated DataFrame:")
+                    st.write(df)
+
+                    # Save the converted data to a file
+                    if st.button("Download Converted Data"):
+                        converted_data_csv = df.to_csv(index=False)
+                        b64 = base64.b64encode(converted_data_csv.encode()).decode()
+                        href = f'<a href="data:file/csv;base64,{b64}" download="converted_data.csv">Download CSV</a>'
+                        st.markdown(href, unsafe_allow_html=True)
+                except Exception as e:
+                    st.error(f"Conversion failed: {str(e)}")
+        else:
+            st.error("File not Upload or Present")
+    elif page == "Plot Graph":
+        # Plot graph page
+        st.title("Plot Graph")
+
+        if uploaded_file is not None:
+            df = pd.read_csv(uploaded_file)
+            selected_columns = st.multiselect("Select columns to plot:", df.columns)
 
             if selected_columns:
-                # Display selected columns
-                st.write("Selected Columns:")
-                st.write(df[selected_columns])
+                chart_type = st.selectbox(
+                    "Select chart type:", ["Line Chart", "Bar Chart"]
+                )
 
-                # Statistical analysis
-                if st.button("Calculate Statistics"):
-                    st.write("Statistics:")
-                    try:
-                        for column in selected_columns:
-                            st.subheader(f"Column: {column}")
-                            st.write(f"Mean: {df[column].mean()}")
-                            st.write(f"Mode: {df[column].mode()[0]}")
-                            st.write(f"Standard Deviation: {df[column].std()}")
-                            st.write(f"Variance: {df[column].var()}")
+                num_rows = st.slider(
+                    "Number of Rows to Show in the Graph",
+                    min_value=1,
+                    max_value=df.shape[0],
+                    value=min(10, df.shape[0]),
+                )
 
-                    except Exception as e:
-                        print(e)
-                        st.error(f"One of the Columns is not Quantitative")
+                y_index = st.selectbox("Select Y-Index", selected_columns)
+                if st.button("Plot Graph"):
+                    data_to_plot = df[selected_columns].head(num_rows)
+                    chart = None
+                    if chart_type == "Line Chart":
+                        df = pd.DataFrame(data_to_plot)
+                        chart = st.line_chart(df.set_index([y_index]))
+                    elif chart_type == "Bar Chart":
+                        df = pd.DataFrame(data_to_plot)
+                        chart = st.bar_chart(df.set_index([y_index]))
+                    else:
+                        st.warning("Select exactly two columns for Scatter Plot.")
+
+                    if chart is not None:
+                        pass
+                    else:
+                        st.warning(
+                            "No chart selected or data columns not compatible with the selected chart type"
+                        )
+        else:
+            st.error("File not Upload or Present")
+
+    elif page == "Download Data":
+        # Download data page
+        st.title("Download Data")
+        st.write("Select data to download:")
+
+        if uploaded_file is not None:
+            df = pd.read_csv(uploaded_file)
+
+            if st.button("Download Data"):
+                # Save the data to a file
+                data_csv = df.to_csv(index=False)
+                b64 = base64.b64encode(data_csv.encode()).decode()
+                href = f'<a href="data:file/csv;base64,{b64}" download="data.csv">Download CSV</a>'
+                st.markdown(href, unsafe_allow_html=True)
+        else:
+            st.error("File not Upload or Present")
 
 
 if __name__ == "__main__":
-    st.set_page_config(
-        page_title="CSVDash",  # Set your desired title here
-        page_icon=":chart_with_upwards_trend:",  # Set your desired favicon (emoji) here
-    )
     st.set_option("deprecation.showfileUploaderEncoding", False)
+    st.set_page_config(
+        page_title="CSVDash",
+        page_icon=":chart_with_upwards_trend:",
+    )
     main()
---
+++ src/home.py
@@ -0,0 +1,20 @@
+# home.py
+
+import streamlit as st
+
+
+def page_home():
+    # Home page content
+    # Define the Streamlit app title and a brief description
+    st.title("CSVdash - Convert & Visualize CSV Data")
+    st.write("Welcome to CSVdash, your tool for converting and visualizing CSV data!")
+
+    st.title("UI Changes:")
+    st.write("1. Updated the color scheme for a more modern look.")
+    st.write("2. Reorganized the navigation menu for better user flow.")
+    st.write("3. Increased font size for improved readability.")
+
+    st.title("User Acceptance Testing (UAT) Progress:")
+    st.write("1. Invited stakeholders to test the application.")
+    st.write("2. Received feedback on UI and functionality.")
+    st.write("3. Identified and fixed several issues reported by users.")
