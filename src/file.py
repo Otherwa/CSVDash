18505988e07c0cfb1d6f511369deaeaf0d7f3bf4
Atharv Desai <atharvdesai2002@gmail.com>
2023-11-01 23:27:00 +0530

    Otherwa (#8)
    * /updates + column qual -> quan, graph + /
    
    * /file+ dir setup 30%/
    
    * /test_data/
    
    * /changes/
    
    * /line/
    
    * /
    + dataframe editor file.py
    + charts file.py
    + components folder
    + moulde folder
    /
    
    * /minor fixes/
    
    * /df fixes/
    
    ---------
    
    Co-authored-by: Otherwa <67428572+Atharv2002@users.noreply.github.com>

--- src/file.py
+++ src/file.py
@@ -8,15 +8,12 @@ import streamlit as st
 # ? Custom Dependeinces
 import pandas as pd
 import plotly.express as px
-
 from src.modules.file.chart_gen import generate_charts
+from src.modules.file.edit_data import edit_data
 
 
-def get_csv_download_link(df):
-    csv = df.to_csv(index=False)
-    b64 = base64.b64encode(csv.encode()).decode()
-    href = f'<a href="data:file/csv;base64,{b64}" download="modified_data.csv">Download CSV</a>'
-    return href
+# Initialize a variable to store the DataFrame
+df = None
 
 
 from src.modules.file.chart_gen import generate_charts
@@ -30,9 +27,12 @@ def get_csv_download_link(df):
 
 
 def page_file(uploaded_file):
+    # ? global dataframe
+    global df
     if uploaded_file is not None:
-        # Read the uploaded CSV file into a DataFrame
-        df = pd.read_csv(uploaded_file, index_col=False)
+        if df is None:
+            #  Read the uploaded CSV file into a DataFrame
+            df = pd.read_csv(uploaded_file, index_col=False)
 
         # Create a selectbox for switching between tabs
         selected_tab = st.selectbox("Select a tab:", ["Details", "Data", "Details"])
@@ -44,15 +44,8 @@ def page_file(uploaded_file):
             # * generate charts
             generate_charts(df)
         elif selected_tab == "Data":
-            # ? Display the DataFrame as a table
-            st.write("Table View:")
-            df = st.data_editor(df, hide_index=True)
-
-            # ? Add a save button
-            if st.button("Download CSV"):
-                # ? Generate a CSV file with a unique name
-                download_link = get_csv_download_link(df)
-                st.markdown(download_link, unsafe_allow_html=True)
+            # * edit csv data
+            edit_data(df)
 
         elif selected_tab == "Details":
             # ? Display the time the DataFrame was created and the encoding
